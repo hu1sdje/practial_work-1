5db455546a8c82361810c4658a401523ff99109b
hu1sdje <159676546+hu1sdje@users.noreply.github.com>
2024-02-16 23:41:55 +0700

    13.py

--- 13.py
+++ 13.py
@@ -1,5 +1,6 @@
 import math
 radius1 = int(input('Введите значение радиуса '))
 radius2 = int(input('Введите значение радиуса '))
-territory = (radius1 ** 2) * math.pi - (radius2 ** 2) * math.pi if radius1 > radius2 else (radius2 ** 2) * math.pi - (radius1 ** 2) * math.pi
+territory = (radius1 ** 2) * math.pi - (radius2 ** 2) * math.pi \
+    if radius1 > radius2 else (radius2 ** 2) * math.pi - (radius1 ** 2) * math.pi
 print(f'Территорияя дальности приёма: {territory}')
